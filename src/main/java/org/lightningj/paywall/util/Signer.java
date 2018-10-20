a96c4cfb7d1c7c1324da0c1249042daa2e3d3e25
Philip Vendil <info@lightningj.org>
2018-10-20 15:17:00 +0200

    Fixed licence header to indicate LGPL 3.0+

--- src/main/java/org/lightningj/paywall/util/Signer.java
+++ src/main/java/org/lightningj/paywall/util/Signer.java
@@ -1,3 +1,17 @@
+/*
+ *************************************************************************
+ *                                                                       *
+ *  LightningJ                                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public License   *
+ *  (LGPL-3.0-or-later)                                                  *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 3 of the License, or any later version.                      *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.lightningj.paywall.util;
 
 import org.lightningj.paywall.InternalErrorException;
